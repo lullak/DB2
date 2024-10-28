de7892fb209669f6bbe424a2237792fac1361ea1
lullak <dennisbircan@gmail.com>
2024-10-28 11:02:09 +0100

    ado1

---
+++ Ado1/FormAdo1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace Ado1
+{
+    partial class FormAdo1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBoxEmp = new ListBox();
+            SuspendLayout();
+            // 
+            // listBoxEmp
+            // 
+            listBoxEmp.FormattingEnabled = true;
+            listBoxEmp.Location = new Point(57, 24);
+            listBoxEmp.Name = "listBoxEmp";
+            listBoxEmp.Size = new Size(712, 404);
+            listBoxEmp.TabIndex = 0;
+            // 
+            // FormAdo1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listBoxEmp);
+            Name = "FormAdo1";
+            Text = "FormAdo1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListBox listBoxEmp;
+    }
+}
---
+++ Ado1/FormAdo1.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Ado1
+{
+    public partial class FormAdo1 : Form
+    {
+        public FormAdo1()
+        {
+            InitializeComponent();
+            LoadAllEmployees();
+        }
+
+        public void LoadAllEmployees()
+        {
+            string connString = "Data Source=localhost;Initial Catalog=Northwind;Integrated Security=SSPI;TrustServerCertificate=True;";
+
+            DataTable dataTable = new DataTable();
+
+            using(SqlConnection conn = new SqlConnection(connString))
+            {
+                string sql = "select employeeid, firstname, lastname from employees";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                adapter.Fill(dataTable);
+            }
+            dataTable.Columns.Add("FullName", typeof(string), "firstname + ' ' + lastname");
+            listBoxEmp.DisplayMember = "FullName";
+            listBoxEmp.ValueMember = "employeeid";
+            listBoxEmp.DataSource = dataTable;
+        }
+    }
+}
